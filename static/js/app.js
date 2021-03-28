e8a7926595e390d06b5e17044c8089e3b8b9af10
Brook Miller <millerbrook@gmail.com>
2021-03-28 16:26:10 -0400

    Initial filter and attempt at outputting to table

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+date = "1/11/2010";
+
+dateFilter = [];
+tableData.forEach(function(item) {
+    if (item.datetime === date)
+        dateFilter.push(item)
+    })
+
+var printTable = d3.select("#ufo-table")
+
+dateFilter.forEach( function(dateObject) {
+    var row = printTable.append("tr");
+    Object.values(dateObject).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
