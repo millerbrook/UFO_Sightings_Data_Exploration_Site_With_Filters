500639db3f8d1b08f08fe24514cc32cd46c43f12
Brook Miller <millerbrook@gmail.com>
2021-04-01 11:58:34 -0400

    Level 2 2 input fields working

--- static/js/app2.js
+++ static/js/app2.js
@@ -13,7 +13,8 @@ var form = d3.select("#date-form");
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
-//Create the function to run for both events
+//Create the function to run for both events...
+//function wraps everything that follows
 function runEnter() {
   //Prevent the page from refreshing
   d3.event.preventDefault();
@@ -21,10 +22,21 @@ function runEnter() {
   //Select the input element and get the raw HTML node
   var dateField = d3.select("#datetime");
   var cityField = d3.select("#cityInput");
+  var stateField = d3.select("#stateInput");
+  var countryField = d3.select("#countryInput");
+  var shapeField = d3.select("#shapeInput");
+  var durationField = d3.select("#durationInput");
+     
 
   //Get the value property of the input element
   var dateValue = dateField.property("value");
   var cityValue = cityField.property("value");
+  var stateValue = stateField.property("value");
+  var countryValue = countryyField.property("value");
+  var shapeValue = shapeField.property("value");
+  var durationValue = durationField.property("value");
+  console.log("Date Value is: ", dateValue);
+  console.log("City value is: ", cityValue);
   //Filter array for final table
   //Set to full data set to start
   tableFiltered = data;
@@ -33,13 +45,13 @@ function runEnter() {
   dateFilter = [];
   if (dateValue.length > 0) {
     tableData.forEach(function (item) {
-        console.log(dateValue);
+        //console.log(dateValue);
         if (item.datetime === dateValue) {
         dateFilter.push(item);
         }
     });
   };
-  console.log(dateFilter);
+  //console.log(dateFilter);
 
   //Check if filter grabbed any values. if so, make tableFiltered the set of those values
   if (dateFilter.length > 0) {
@@ -51,16 +63,18 @@ function runEnter() {
 
   //If cityValue.length > 0, filter for city
   if (cityValue.length > 0) {
-  tableData.forEach(function (item) {
-    console.log(dateValue);
+  tableFiltered.forEach(function (item) {
+    //console.log(cityValue);
     if (item.city === cityValue) {
       cityFilter.push(item);
     }
     });
-    };
-  console.log(dateFilter);
-  console.log(cityFilter);
-  console.log(tableFiltered);
+    tableFiltered = cityFilter;  
+  };
+  
+    //console.log(dateFilter);
+  //console.log(cityFilter);
+  //console.log(tableFiltered);
 //Possibility: none of the filters narrowed down tableFiltered, so nothing should be output
 if (tableFiltered === data) {
     //PRINT "No Records Found" to "#no-output" div (tucked below... or put something in an make it hidden, then change to visible?)
@@ -80,3 +94,4 @@ else {
   });
 };
 };
+// need to delete old table if a new search is conducted
