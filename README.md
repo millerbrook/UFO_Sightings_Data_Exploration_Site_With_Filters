5dd790278d459d998659042f7e7ddae452514573
Brook Miller <millerbrook@gmail.com>
2021-05-07 21:45:41 -0400

    Updates README file for portfolio

--- README.md
+++ README.md
@@ -1,41 +1,49 @@
-# javascript-challenge
+# Data Exploration Site with Filters
 
- Create a new repository for this project called `javascript-challenge`.--DONE
 
-2. Clone the new repository to your computer. --DONE
+## Overview
+This project creates a styled website with an interactive table based on a dataset of reported UFO sightings. Uses javascript (and d3), html, and css.
 
-3. Inside your local git repository, create a directory for the Javascript challenge. Use the folder names to correspond to the challenges: **UFO-level-1** and **UFO-level-2**.--DONE
+## Data
+Stored as a javascript array: [UFO Sightings Data](static/js/data.js).
 
-4. Add your **html** files to this folder as well as your static folder containing your javascript. This will be the main script to run for analysis.--DONE
+## Steps
 
-5. Push the above changes to GitHub or GitLab.--DONE
+### Step 1
+Create an html page with ids and classes organized to visualize the data and listen for filtering events.
 
-6. Ensure your repository has regular commits (i.e. 20+ commits) and a thorough README.md file --13 COMMITS
+### Step 2
+Build a table into the html file using D3:
+```
+function buildHTMLTable(incomingData) {
+  //remove leftover rows
+  var oldRows = d3.select("tbody").selectAll("tr");
+  oldRows.remove();
+  //Print table
+  var printTable = d3.select("tbody");
+  incomingData.forEach(function (tableObject) {
+    //Make a row for each object in table
+    var row = printTable.append("tr");
+    //Print values within each object
+    Object.entries(tableObject).forEach(([key, value]) => {
+      var cell = row.append("td");
+      console.log(value);
+      cell.text(value);
+    });
+  });
+}
+```
+### Step 3
+Build input filters for users to search for certain records. 
 
-### Level 1: Automatic Table and Date Search (Required)
+![Table with Filters](static/images/filters.png)
 
-* Create a basic HTML web page or use the [index.html](StarterCode/index.html) file provided (we recommend building your own custom page!). --DONE
+Use d3 to process events by filtering the data and rebuilding the table.
 
-* Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting. --DONE
+## Contact Information
 
-  * Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least. --MUCH WAS PRE-SUPPLIED
+Brook Miller
 
-* Use a date form in your HTML document and write JavaScript code that will listen for events and search through the `date/time` column to find rows that match user input.--DONE
+[Email](millerbrook@gmail.com)
 
-### Level 2: Multiple Search Categories (Optional)
-
-* Complete all of Level 1 criteria.--DONE
-
-* Using multiple `input` tags and/or select dropdowns, write JavaScript code so the user can to set multiple filters and search for UFO sightings using the following criteria based on the table columns:
-
-  1. `date/time`
-  2. `city`
-  3. `state`
-  4. `country`
-  5. `shape`
-
-- - -DONE
-
-### Dataset
-
-* [UFO Sightings Data](StarterCode/static/js/data.js)
+[LinkedIn](www.linkedin.com/in/brook-miller-data)
--- static/js/app2.js
+++ static/js/app2.js
@@ -13,7 +13,6 @@ var form = d3.select("#date-form");
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
-
 function buildHTMLTable(incomingData) {
   //remove leftover rows
   var oldRows = d3.select("tbody").selectAll("tr");
@@ -30,7 +29,8 @@ function buildHTMLTable(incomingData) {
       cell.text(value);
     });
   });
-}
+};
+
 //Create the function to run for both events...
 //function wraps everything that follows
 function runEnter() {
@@ -44,7 +44,6 @@ function runEnter() {
   var countryField = d3.select("#countryInput");
   var shapeField = d3.select("#shapeInput");
   var durationField = d3.select("#durationInput");
-     
 
   //Get the value property of the input element
   var dateValue = dateField.property("value");
@@ -55,6 +54,7 @@ function runEnter() {
   var durationValue = durationField.property("value");
   console.log("Date Value is: ", dateValue);
   console.log("City value is: ", cityValue);
+
   //Filter array for final table
   //Set to full data set to start
   tableFiltered = data;
@@ -63,98 +63,98 @@ function runEnter() {
   let dateFilter = [];
   if (dateValue.length > 0) {
     tableData.forEach(function (item) {
-        //console.log(dateValue);
-        if (item.datetime === dateValue) {
+      //console.log(dateValue);
+      if (item.datetime === dateValue) {
         dateFilter.push(item);
-        }
+      }
     });
-  };
+  }
   //console.log(dateFilter);
 
   //Check if filter grabbed any values. if so, make tableFiltered the set of those values
   if (dateFilter.length > 0) {
     tableFiltered = dateFilter;
-  };
+  }
 
-  //establish array to store filtered by city data
+  //Establish array to store filtered by city data
   let cityFilter = [];
 
   //If cityValue.length > 0, filter for city
   if (cityValue.length > 0) {
-  tableFiltered.forEach(function (item) {
-    //console.log(cityValue);
-    if (item.city === cityValue) {
-      cityFilter.push(item);
-    }
+    tableFiltered.forEach(function (item) {
+      //console.log(cityValue);
+      if (item.city === cityValue) {
+        cityFilter.push(item);
+      }
     });
-    tableFiltered = cityFilter;  
-  };
+    tableFiltered = cityFilter;
+  }
 
-     //establish array to store filtered by state data
-     let stateFilter = [];
+  //Establish array to store filtered by state data
+  let stateFilter = [];
 
-     //If state field has a value, filter for state
-     if (stateValue.length > 0) {
-     tableFiltered.forEach(function (item) {
-       //console.log(stateValue);
-       if (item.state === stateValue) {
-         stateFilter.push(item);
-       }
-       });
-       tableFiltered = stateFilter;  
-     };
-     
-    //establish array to store filtered by country data
-    let countryFilter = [];
+  //If state field has a value, filter for state
+  if (stateValue.length > 0) {
+    tableFiltered.forEach(function (item) {
+      //console.log(stateValue);
+      if (item.state === stateValue) {
+        stateFilter.push(item);
+      }
+    });
+    tableFiltered = stateFilter;
+  }
 
-    //If state field has a value, filter for country
-    if (countryValue.length > 0) {
-      tableFiltered.forEach(function (item) {
+  //Establish array to store filtered by country data
+  let countryFilter = [];
+
+  //If state field has a value, filter for country
+  if (countryValue.length > 0) {
+    tableFiltered.forEach(function (item) {
       //console.log(stateValue);
       if (item.country === countryValue) {
         countryFilter.push(item);
       }
-      });
-      tableFiltered = countryFilter;  
-    };
-    
-    //establish array to store filtered by shape data
-    let shapeFilter = [];
+    });
+    tableFiltered = countryFilter;
+  }
 
-    //If state field has a value, filter for shape
-    if (shapeValue.length > 0) {
-      tableFiltered.forEach(function (item) {
+  //Establish array to store filtered by shape data
+  let shapeFilter = [];
+
+  //If state field has a value, filter for shape
+  if (shapeValue.length > 0) {
+    tableFiltered.forEach(function (item) {
       //console.log(shapeValue);
       if (item.shape === shapeValue) {
         shapeFilter.push(item);
       }
-      });
-      tableFiltered = shapeFilter;  
-    };
+    });
+    tableFiltered = shapeFilter;
+  }
 
-    //establish array to store filtered by duration data
-    let durationFilter = [];
+  //Establish array to store filtered by duration data
+  let durationFilter = [];
 
-    //If state field has a value, filter for duration
-    if (durationValue) {
-      tableFiltered.forEach(function (item) {
+  //If state field has a value, filter for duration
+  if (durationValue) {
+    tableFiltered.forEach(function (item) {
       //console.log(stateValue);
       if (item.durationMinutes === durationValue) {
         durationFilter.push(item);
       }
-      });
-      tableFiltered = durationFilter;  
-    };
-    //console.log(dateFilter);
+    });
+    tableFiltered = durationFilter;
+  }
+  //console.log(dateFilter);
   //console.log(cityFilter);
   //console.log(tableFiltered);
-//Possibility: none of the filters narrowed down tableFiltered, so nothing should be output
-if (tableFiltered === data) {
-    //PRINT "No Records Found" to "#no-output" div (tucked below... or put something in an make it hidden, then change to visible?)
+
+  //Possibility: none of the filters narrowed down tableFiltered, so original table should be output
+  if (tableFiltered === data) {
+    buildHTMLTable(tableData);
+  } else {
+    buildHTMLTable(tableFiltered);
+  }
 }
-else {
-  buildHTMLTable(tableFiltered);
-};
-};
 
 buildHTMLTable(tableData);
